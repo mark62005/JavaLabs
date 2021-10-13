538fddc0a47811811214001abdb502c3c3cb5730
Mark Wong <markwong.digital@gmail.com>
2021-10-13 16:26:41 -0700

    Add the third constructor for Model

--- src/lab1/Model.java
+++ src/lab1/Model.java
@@ -27,4 +27,22 @@ public class Model {
         this.smokes = smokes;
     }
 
+    /**
+     * third constructor: takes parameters for only the first name, last name, height in
+     * inches, and weight in pounds, and set canTravel and smokes to false
+     *
+     * @param firstName first name
+     * @param lastName last name
+     * @param height height in inches
+     * @param weight weight in pounds
+     */
+    public Model(String firstName, String lastName, int height, int weight) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.height = height;
+        this.weight = weight;
+        this.canTravel = false;
+        this.smokes = false;
+    }
+
 }
