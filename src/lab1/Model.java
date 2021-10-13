5a2556e12aa6ee47e8d57ee660319b1b8bb1cadc
Mark Wong <markwong.digital@gmail.com>
2021-10-13 16:13:20 -0700

    Add data fields for Model Class

--- src/lab1/Model.java
+++ src/lab1/Model.java
@@ -1,4 +1,13 @@
 package lab1;
 
 public class Model {
+
+    // data fields
+    private String firstName;
+    private String lastName;
+    private int height; // inches
+    private int weight; // pounds
+    private boolean canTravel;
+    private boolean smokes;
+
 }
