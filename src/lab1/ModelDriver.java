5371b74264e973e01317787f77f70d03dc055485
Mark Wong <markwong.digital@gmail.com>
2021-10-13 20:02:24 -0700

    Test Model with ModelDriver

--- src/lab1/ModelDriver.java
+++ src/lab1/ModelDriver.java
@@ -1,4 +1,15 @@
 package lab1;
 
 public class ModelDriver {
+    public static void main(String[] args) {
+
+        Model model1 = new Model();
+        Model model2 = new Model("John", "Doe", 80, 200, true, true);
+        Model model3 = new Model("John", "Doe", 50, 100.30);
+
+        model1.printDetails();
+        model2.printDetails();
+        model3.printDetails();
+
+    }
 }
