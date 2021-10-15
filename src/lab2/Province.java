bda2cb413eb19018431efe9d00a1cccb7e4521ab
Mark Wong <markwong.digital@gmail.com>
2021-10-14 18:38:31 -0700

    Add isValidProvince method

--- src/lab2/Province.java
+++ src/lab2/Province.java
@@ -1,6 +1,14 @@
 package lab2;
 
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.Objects;
+
 public class Province {
+
+    private final String[] PROVINCES = {"ontario", "quebec", "british columbia", "alberta", "manitoba",
+            "saskatchewan", "nova scotia", "new brunswick", "newfoundland and labrador",
+            "prince edward island", "northwest territories", "nunavut", "yukon"};
 
     private String name;        // e.g. “British Columbia”
     private String capital;     // e.g. “Victoria”
@@ -10,6 +18,16 @@ public class Province {
         this.name = name;
         this.capital = capital;
         this.population = population;
+    }
+
+    /**
+     * Check if the province name input belongs to one of the province in Canada.
+     *
+     * @param province province name
+     * @return true if it is one of the Canadian provinces
+     */
+    private boolean isValidProvince(String province) {
+        return (Arrays.asList(PROVINCES).contains(province.toLowerCase(Locale.ROOT)));
     }
 
 }
