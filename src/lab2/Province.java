da21d9ee28a5e84ad40ee7b09473db813e8a045a
Mark Wong <markwong.digital@gmail.com>
2021-10-14 18:20:23 -0700

    Add constructor for Province

--- src/lab2/Province.java
+++ src/lab2/Province.java
@@ -1,4 +1,15 @@
 package lab2;
 
 public class Province {
+
+    private String name;        // e.g. “British Columbia”
+    private String capital;     // e.g. “Victoria”
+    private long population;    // e.g. 2264823
+
+    public Province(String name, String capital, long population) {
+        this.name = name;
+        this.capital = capital;
+        this.population = population;
+    }
+
 }
